575f0bc4937b57ba4fa1875c113408fea1029a9c
Bruno Luís <99904338+BrunoLuisUFPB@users.noreply.github.com>
2023-02-06 23:03:29 -0300

    Create Primeiro caractere unico.py

---
+++ Primeiro caractere unico.py
@@ -0,0 +1,5 @@
+#Descrição:
+#Vamos supor que você recebeu uma lista de Strings. É correto afirmar que variás Strings possuem caracteres únicos, como por exemplo: "TAIL" possui 4 caracteres únicos pois nenhum deles se repete, além de que o primeiro caractere único é "T". Exerça sua lógica para desenvolver um algoritmo que identifica o primeiro caractere único dentro de uma String.
+
+#Tarefa:
+#Implemente um script em Python para retornar o índice do primeiro caractere único dentro de uma String. Se o caractere único não existir, retorne -1.
